9678f3eea169d0fca90641c429783593597ee4e8
Divyansh chaudhary <divyanshchaudhary174@gmail.com>
2026-01-03 23:28:07 +0530

    Add files via upload

---
+++ assingment1,task1.py
@@ -0,0 +1,14 @@
+
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+sum = float(num1) + float(num2)
+print("The sum of {0} and {1} is {2}".format(num1, num2, sum))
+
+difference = float(num1) - float(num2)
+print("The difference of {0} and {1} is {2}".format(num1, num2, difference))
+
+product = float(num1) * float(num2)
+print("The product of {0} and {1} is {2}".format(num1, num2, product))  
+
+division = float(num1) / float(num2)
+print("The division of {0} and {1} is {2}".format(num1, num2, division))
