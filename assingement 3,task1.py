b7db6ad7eeed8251e6b7ab82056133ca2dcded15
Divyansh chaudhary <divyanshchaudhary174@gmail.com>
2026-02-05 22:19:23 +0530

    Update assingement 3,task1.py

--- assingement 3,task1.py
+++ assingement 3,task1.py
@@ -1,5 +1,9 @@
-def factorial(n):
-    if n == 0 or n == 1:
+
+def factorial (n):
+    if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
+    
+number = int(input("Enter a number: "))
+print(f"The factorial of {number} is, {factorial(number)}")
